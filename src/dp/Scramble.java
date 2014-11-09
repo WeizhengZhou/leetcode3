7a9ca1e99c251361ac7e6fd799daab597f6e3258
wz33 <zhou66.ustc@gmail.com>
2014-11-09 13:35:20 -0500

    m

---
+++ src/dp/Scramble.java
@@ -0,0 +1,16 @@
+package dp;
+
+public class Scramble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public boolean isScramble(String s1, String s2){
+		return helper(s1,l1,r1,s2,l2,r2);
+	}
+	
+	
+
+}
---
+++ src/dp/Triangle.java
@@ -0,0 +1,41 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<List<Integer>> test=new ArrayList<List<Integer>>();
+		
+
+	}
+	
+	public int minPathSum(List<List<Integer>> list){
+		int level=list.size();
+		List<List<Integer>> res=new ArrayList<List<Integer>>(level);
+		for(int i=0;i<level;i++){
+			List<Integer> l=new ArrayList<Integer>(i+1);
+			for(int j=0;j<=i;j++){
+				l.add(Integer.MAX_VALUE);
+			}
+			res.add(l);
+		}
+		return helper(list,0,0,level, res);
+	}
+	
+	public int helper(List<List<Integer>> list, int l,int index,int tl,List<List<Integer>> res){
+		if (res.get(l).get(index)!=Integer.MAX_VALUE){
+			return res.get(l).get(index);
+		}
+		if (l==tl-1){
+			int num=list.get(l).get(index);
+			return num;
+		}
+		int e=Math.min(helper(list,l+1,index,tl,res), helper(list,l+1,index+1,tl,res))+list.get(l).get(index);
+		res.get(l).set(index, e);
+		return res.get(l).get(index);
+	}
+
+}
