7f608a2a85f16dc9ae3eccdb6d73fe98e87b1296
wz33 <zhou66.ustc@gmail.com>
2014-11-10 23:45:41 -0500

    m

--- src/dp/WordLadderII.java
+++ src/dp/WordLadderII.java
@@ -12,15 +12,15 @@ public class WordLadderII {
 		// TODO Auto-generated method stub
 		WordLadderII w=new WordLadderII();
 		Set<String> dict=new HashSet();
-		dict.add("dog");
 		dict.add("hot");
-		dict.add("dot");
-		dict.add("lot");
-		dict.add("log");
+		dict.add("dog");
+		//dict.add("c");
+		//dict.add("lot");
+		//dict.add("log");
 		
 		//System.out.println("sas".substring(0,1)+(char)('a'+25)+"sas".substring(2));
 				
-		List<List<String>> ll=w.findLadder("hit", "cog", dict);
+		List<List<String>> ll=w.findLadder("hot", "dog", dict);
 		if(ll!=null){
 		for(List<String>l:ll){
 			System.out.println(l.toString());
@@ -30,15 +30,16 @@ public class WordLadderII {
 	}
 	
 	public List<List<String>> findLadder(String start, String end, Set<String> dict){
-		if(start==null||end==null||start.length()!=end.length()){
+		if(start==null||end==null||start.length()!=end.length()||start.length()==0||end.length()==0){
 			return null;
 		}
 		LinkedList<String> cur=new LinkedList<String>();
 		HashMap<String,LinkedList<String>> map=new HashMap<String,LinkedList<String>>();
 		cur.add(start);
 		while(!cur.isEmpty()){
-			LinkedList<String> next=new LinkedList<String>();			
-			while(!cur.isEmpty()){
+			LinkedList<String> next=new LinkedList<String>();	
+			Set<String> newAdd=new HashSet<String>();
+			while(!cur.isEmpty()){				
 				String w=cur.remove();
 				for(int i=0; i<w.length();i++){
 					for(int j=0;j<26;j++){
@@ -46,24 +47,29 @@ public class WordLadderII {
 						if(!temp.equals(w)&&dict.contains(temp)){
 							if(!map.containsKey(temp)){
 								next.add(temp);
+								newAdd.add(temp);
 								LinkedList<String> l=new LinkedList<String>();
 								l.add(w);
 								map.put(temp, l);
 							}
 							else{
-								map.get(temp).add(w);
+								if(newAdd.contains(temp)&&!map.get(temp).contains(w)){
+									map.get(temp).add(w);
+								}
 							}						
 						}
 						if(temp.equals(end)){
 							if(!map.containsKey(temp)){
 								LinkedList<String> l=new LinkedList<String>();
 								l.add(w);
+								newAdd.add(temp);
 								map.put(end, l);
 							}
 							else{
-								map.get(end).add(w);
-							}
-			
+								if(newAdd.contains(temp)&&!map.get(temp).contains(w)){
+									map.get(end).add(w);
+								}
+							}			
 						}
 					}
 					
@@ -81,11 +87,15 @@ public class WordLadderII {
 		}
 		if(map.containsKey(end)){
 			List<List<String>> res=new LinkedList<List<String>>();
-			buildRes(map,res,end,start,new LinkedList<String>());
+			LinkedList<String> list=new LinkedList<String>();
+			list.add(end);
+			buildRes(map,res,end,start,list);
 			return res;
 		}
 		else{
-			return null;
+			List<List<String>> r=new LinkedList<List<String>>();
+			r.add(null);
+			return r;
 		}
 	}
 
