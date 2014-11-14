a90bcbb58997def934a744bb0b8f6a86fe3980d6
wz33 <zhou66.ustc@gmail.com>
2014-11-13 20:51:43 -0500

    m

--- src/dp/Combination.java
+++ src/dp/Combination.java
@@ -1,10 +1,34 @@
 package dp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Combination {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public List<List<Integer>> combine(int n, int k){
+		if (n==0||k==0){
+			return null;
+		}
+		List<List<Integer>> res=new LinkedList<List<Integer>>();
+		helper(1,n,k,res,new LinkedList<Integer>());
+		return res;
+	}
+	
+	public void helper(int index, int n, int k, List<List<Integer>> res, List<Integer> list){
+		if(k==0){
+			res.add(new LinkedList<Integer>(list));
+			return;
+		}
+		for(int i=index; i<=n; i++){
+			list.add(i);
+			helper(i+1,n,k-1,res,list);
+			list.remove(list.size()-1);
+		}
 	}
 
 }
---
+++ src/dp/CombinationSum.java
@@ -0,0 +1,47 @@
+package dp;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public List<List<Integer>> findComSum(int[] candidates, int target){
+		if(candidates==null||candidates.length==0){
+			return null;
+		}
+		Arrays.sort(candidates);
+		List<List<Integer>> res=new LinkedList<List<Integer>>();
+		helper(0,candidates,target,res,new LinkedList<Integer>());
+		return res;
+	}
+	
+	public void helper(int index,int[] can, int t, List<List<Integer>> res,LinkedList<Integer> list ){
+		if(t==0){
+			res.add(new LinkedList<Integer>(list));
+			return;
+		}
+		if(index>=can.length||t<0||can[index]>t){
+			return;
+		}
+		for(int k=index;k<can.length;k++){
+			int target=t;
+			int i=0;
+			while(target>0){
+				list.add(can[k]);
+				target-=can[k];
+				helper(k+1,can,target,res,list);
+			}
+			for(int j=0; j<i;j++){
+				list.remove(list.size()-1);
+			}
+		}
+		
+	}
+
+}
