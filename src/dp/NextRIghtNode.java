208d47b4d0fc6f61f4d772979f02ae929d309774
wz33 <zhou66.ustc@gmail.com>
2014-11-14 22:30:56 -0500

    m

--- src/dp/NextRIghtNode.java
+++ src/dp/NextRIghtNode.java
@@ -12,9 +12,17 @@ public class NextRIghtNode {
 			return;
 		}
 		root.next=null;
-		TreeLinkNode next=root.left;
+		TreeLinkNode next=root;
 		while(next!=null){
 			TreeLinkNode cur=next;
+			next=cur.left;
+			if(next!=null){
+				while(cur!=null){
+					cur.left.next=cur.right;
+					cur.right.next=cur.next==null?null:cur.next.left;
+					cur=cur.next;
+				}
+			}
 			
 		}
 	}
