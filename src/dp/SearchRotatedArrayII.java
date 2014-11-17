c5cb5f211045ff7f52b672c62f2d7d2c3903bece
wz33 <zhou66.ustc@gmail.com>
2014-11-16 20:51:25 -0500

    m

--- src/dp/SearchRotatedArrayII.java
+++ src/dp/SearchRotatedArrayII.java
@@ -6,5 +6,50 @@ public class SearchRotatedArrayII {
 		// TODO Auto-generated method stub
 
 	}
+	
+    public boolean search(int[] A, int target) {
+        if (A==null||A.length==0){
+        	return false;
+        }
+        return helper(A,0,A.length-1,target);
+    }
+    public boolean helper(int[] A, int l, int r, int target){
+    	if (l>r){
+    		return false;
+    	}
+    	if (l==r){
+    		if(A[l]==target){
+    			return true;
+    		}
+    		return false;
+    	}
+    	int m=l+(r-l)/2;
+    	if (A[m]==target){
+    		return true;
+    	}
+    	if (A[l]<A[m]){
+    		if (target<A[m]&& target>=A[l])
+    		{
+    			return helper(A,l,m-1,target);
+    		}
+    		else{
+    			return helper(A,m+1,r,target);
+    		}    		
+    	}
+    	else if (A[l]>A[m]){
+    		if (target>A[m]&&target<=A[r])
+    		{
+    			return helper(A,m+1,r,target);
+    		}
+    		else{
+    			return helper(A,l,m-1,target);
+    		}
+    	}
+    	else{
+    		boolean left=helper(A,l,m-1,target);
+    		return left?true:helper(A,m+1,r,target);
+    	}
+    	
+    }
 
 }
