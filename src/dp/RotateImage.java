e8d47389a47e996c191f5709d19219be0a1fc90c
wz33 <zhou66.ustc@gmail.com>
2014-11-16 22:04:17 -0500

    m

--- src/dp/RotateImage.java
+++ src/dp/RotateImage.java
@@ -11,7 +11,17 @@ public class RotateImage {
     	if (matrix==null||matrix.length==0||matrix[0]==null||matrix[0].length==0){
     		return;
     	}
-    	
+    	int n=matrix.length;
+    	for(int d=0;d<n/2;d++){
+    		for(int i=d;i<n-1-d;i++){
+    			int temp=matrix[d][i];
+    			matrix[d][i]=matrix[n-1-i][d];
+    			matrix[n-1-i][d]=matrix[n-1-d][n-1-i];
+    			matrix[n-1-d][n-1-i]=matrix[i][n-1-d];
+    			matrix[i][n-1-d]=temp;   			    			
+    		}
+    	}
+    	return;
     }
 
 }
