66c6d965e0d54c092781dc01a20b92efe7774aa5
wz33 <zhou66.ustc@gmail.com>
2014-11-22 16:04:54 -0500

    m

---
+++ src/dp/SwapNodePairs.java
@@ -0,0 +1,16 @@
+package dp;
+
+public class SwapNodePairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public ListNode swapPairs(ListNode head) {
+        if(head==null){
+        	return null;
+        }
+        ListNode dummy=new ListNode(0);
+        
+    }
+}
