53481b1ea121d103562378c55ae6a919bda8a65a
wz33 <zhou66.ustc@gmail.com>
2014-11-28 22:59:37 -0500

    m

--- src/dp/SingleNumberII.java
+++ src/dp/SingleNumberII.java
@@ -4,8 +4,9 @@ public class SingleNumberII {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(Integer.toBinaryString(-1));
 		SingleNumberII s=new SingleNumberII();
-		int i=s.singleNumber(new int[]{-2,-2,-3,-2});
+		int i=s.singleNumber(new int[]{-4,-4,-1,-4});
 		System.out.println(i);
 	}
     public int singleNumber(int[] A) {
@@ -13,30 +14,27 @@ public class SingleNumberII {
         	return 0;
         }
         int ones=0;
-        int temp=0;
+        int carry=0;
+        int mask=0;
         int twos=0;
         //int s=1;
-        int c=0;
+        //int c=0;
         for(int i=0;i<A.length;i++){
-        	if(A[i]<0){
-        		c++;
-        		//s=s*(-1);
-        	}
-        	int a=Math.abs(A[i]);
-        	temp=ones&a;
+        	int a=A[i];
+        	carry=ones&a;
+        	//mask=carry^twos;
+        	ones=(a^ones);
+        	twos=twos^carry;
+        	mask = ones&twos;
+        	ones = ones^mask;
+        	twos = twos^mask;
         	//System.out.print("temp="+Integer.toBinaryString(temp));
-        	ones=ones^a;
+        	//ones=ones^a;
         	//System.out.print(",ones="+Integer.toBinaryString(ones));
-        	twos=temp^twos;        	
+        	//twos=carry^twos;        	
         	//System.out.print(",twos="+Integer.toBinaryString(twos));
         	//System.out.println("");
         }
-        if(c%3==0)
-        {
-        	return temp+ones;
-        }
-        else{
-        	return -(temp+ones);
-        }
+        return ones;
     }
 }
