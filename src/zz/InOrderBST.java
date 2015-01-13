dae75e49caa72e906c2bf2d5412bacc1d9d6e575
wz33 <zhou66.ustc@gmail.com>
2015-01-12 22:12:26 -0500

    m

---
+++ src/zz/InOrderBST.java
@@ -0,0 +1,35 @@
+package zz;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class InOrderBST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res=new LinkedList<Integer>();
+		if(root==null){
+			return res;
+		}
+		TreeNode current=root;
+		Stack<TreeNode> stack=new Stack<TreeNode>();
+		while(current!=null || !stack.isEmpty()){
+			if(current!=null){
+				stack.push(current);
+				current=current.left;
+			}
+			else{
+				TreeNode top=stack.pop();
+				res.add(top.val);
+				current=top.right;
+			}
+		}
+		return res;
+	}
+
+}
---
+++ src/zz/ValidIP.java
@@ -0,0 +1,10 @@
+package zz;
+
+public class ValidIP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
