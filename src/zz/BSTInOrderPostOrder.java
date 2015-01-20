b1611b9f3a1b7b446932fc6f40e18b63f0942cdb
WeizhengZhou <zhou66.ustc@gmail.com>
2015-01-20 11:09:43 -0500

    review

--- src/zz/BSTInOrderPostOrder.java
+++ src/zz/BSTInOrderPostOrder.java
@@ -1,3 +1,5 @@
+
+//zz reviewd
 package zz;
 
 import java.util.HashMap;
@@ -16,6 +18,9 @@ public class BSTInOrderPostOrder {
     	if(inorder==null || postorder==null || inorder.length==0 || inorder.length!=postorder.length){
     		return null;
     	}
+		//zz, if the tree has duplicates, this method will fail
+		//zz, consider given inorder = {1,1}, postorder = {1,1}
+		//zz, the hashmap will only have one element, however ~~~
     	Map<Integer,Integer> map=new HashMap<Integer,Integer>();
     	for(int i=0;i<inorder.length;i++){
     		map.put(inorder[i], i);
@@ -24,15 +29,17 @@ public class BSTInOrderPostOrder {
     }
     
     public TreeNode helper(int inLeft, int inRight, int[] inorder, int postLeft, int postRight, int[] postorder, Map<Integer, Integer> map){
+        
     	if(inLeft>inRight || postLeft>postRight){
     		return null;
     	}
+		//zz, should check if(inRight-inLeft == postRight-postLeft)
     	if(inLeft==inRight){
-    		return new TreeNode(inorder[inLeft]);
+    		return new TreeNode(inorder[inLeft]);//zz before you call inorder[inLeft], you should check if inLeft out of boundary
     	}
     	TreeNode root=new TreeNode(postorder[postRight]);
-    	int index=map.get(postorder[postRight]);
-    	int postLeftLength=index-inLeft;
+    	int index=map.get(postorder[postRight]);//zz, index renamed to rootIndex
+    	int postLeftLength=index-inLeft;//zz, postLeftLength renamed to postLeftSubTreeLength
     	int postRightLength=inRight-index;
     	//System.out.println("index="+index+",postleftLen="+postLeftLength+",postRightLen="+postRightLength);
     	//System.out.println("inLeft="+inLeft+",inright="+inRight);
